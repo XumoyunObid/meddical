806833910ac224f9b620484ddcf188f426fb415f
Nusratillo <nxursanjonov888@gmail.com>
2024-02-01 17:07:13 +0500

    Structure add

---
+++ src/App.jsx
@@ -0,0 +1,27 @@
+import { Routes, Route } from "react-router-dom";
+import { MainLayout } from "./layout/main-layout";
+import { nanoid } from "nanoid";
+import { main_pages } from "./router/main-router";
+
+// import { About } from "./pages/about";
+
+function App() {
+  return (
+    <>
+      <Routes>
+        <Route path="/" element={<MainLayout />}>
+          {main_pages.map((route) => (
+            <Route
+              index={route.path ? false : true}
+              key={nanoid()}
+              path={route.path}
+              element={route.component}
+            />
+          ))}
+        </Route>
+      </Routes>
+    </>
+  );
+}
+
+export default App;
---
+++ src/router/main-router.jsx
@@ -0,0 +1,33 @@
+export const main_pages = [
+  {
+    component: <Home />,
+  },
+  {
+    component: <About />,
+    path: "about",
+  },
+  {
+    component: <Services />,
+    path: "services",
+  },
+  {
+    component: <Doctors/>,
+    path: "doctors"
+  },
+  {
+    component: <Contact/>,
+    path: "contact"
+  },
+  {
+    component: <Appointment/>,
+    path: "appointment"
+  },
+  {
+    component: <News/>,
+    path: "news"
+  },
+  {
+    component: <SingleNew/>,
+    path: "singleNew"
+  }
+];
