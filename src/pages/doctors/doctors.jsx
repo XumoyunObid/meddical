f3ba374a91ec391c7f0876eefece6466c54acd95
Nusratillo <nxursanjonov888@gmail.com>
2024-02-02 11:27:10 +0500

    afmlasklsakad

--- src/pages/doctors/doctors.jsx
+++ src/pages/doctors/doctors.jsx
@@ -1,7 +1,19 @@
 import React from 'react'
+import { AboutHero, JohnDoe } from '../about/About'
+
+export const DoctorsCard = ()=> {
+  try {
+    return <div className="doctorsCard">
+      
+    </div>
+  } catch (e) {console.log(e);}
+}
 
 export const Doctors = () => {
   return (
-    <div>Doctors</div>
+    <section className='doctors'>
+      <AboutHero pageName={'Doctors'} pageTitle={'Our Doctors'} />
+      <JohnDoe />
+    </section>
   )
 }
