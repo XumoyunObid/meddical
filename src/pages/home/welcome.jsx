c877b9586e53f4376b27de82754307550a3fec86
azizdjan_official <tulaganovazizjon@gmail.com>
2024-02-02 14:14:24 +0500

    changed medical

--- src/pages/home/welcome.jsx
+++ src/pages/home/welcome.jsx
@@ -2,11 +2,13 @@ import "./../home/home.css";
 
 const Welcome = () => {
   return (
-    <div className="welcome">
+    <div>
+      <div className="welcome">
         <h1>Welcome to Meddical</h1>
         <h2>A Great Place to Receive Care</h2>
         <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque placerat scelerisque tortor ornare ornare. Convallis felis vitae tortor augue. Velit nascetur proin massa in. Consequat faucibus porttitor enim et.</p>
         <a href="#">Learn More </a>
+    </div>
     </div>
   )
 }
