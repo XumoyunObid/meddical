1c2724e2a89996c0b298831738318814307af599
Jasurbek <jaskasamadov695@gmail.com>
2024-02-03 08:28:49 +0500

    medic191

--- src/pages/appointment/appointment.jsx
+++ src/pages/appointment/appointment.jsx
@@ -1,7 +1,28 @@
 import React from "react";
-// import img from "../../assets/appoinment-img/Rectangle31";
+import img from "../../assets/appoinment-img/Rectangle31.png";
+import img1 from "../../assets/appoinment-img/Frame2.png"
+import "./appointments.css"
 import { Link } from "react-router-dom";
 
 export const Appointments = () => {
-  return <div></div>;
+  return (
+    <div className="appoinments">
+      <div className="appoinments-hero">
+        <img className="appoinments-hero-img" src={img} alt="" />
+        <img className="appoinments-hero-img1" src={img1} alt="" />
+        <div className="container">
+          <div className="appointments-hero-title">
+            <Link to="/">
+              <span className="appointments-text">Home</span>
+            </Link>
+            /
+            <Link to="#">
+              <span className="appointments-text">Appointment</span>
+            </Link>
+            <h1 className="appointments-title">Book an Appointment</h1>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
