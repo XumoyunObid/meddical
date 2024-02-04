5215ab4913dbe83dd350274d91870b883e322a60
Jasurbek <jaskasamadov695@gmail.com>
2024-02-04 12:13:37 +0500

    medic1911

--- src/pages/appointment/appointment.jsx
+++ src/pages/appointment/appointment.jsx
@@ -1,7 +1,11 @@
 import React from "react";
 import img from "../../assets/appoinment-img/Rectangle31.png";
-import img1 from "../../assets/appoinment-img/Frame2.png"
-import "./appointments.css"
+import img1 from "../../assets/appoinment-img/Frame2.png";
+import { AppGender } from "./companents-app/app-gender";
+import { AppShedul } from "./companents-app/app-shedul";
+import { IframeComponent } from "../contacts/contact-maps";
+import Touch from "../home/Touch";
+import "./appointments.css";
 import { Link } from "react-router-dom";
 
 export const Appointments = () => {
@@ -21,6 +25,25 @@ export const Appointments = () => {
             </Link>
             <h1 className="appointments-title">Book an Appointment</h1>
           </div>
+          <div className="shedule">
+            <div className="appointment-shedul">
+              <h1>Book an Appointment</h1>
+              <p>
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque
+                placerat scelerisque tortor ornare ornare. Convallis felis vitae
+                tortor augue. Velit nascetur proin massa in. Consequat faucibus
+                porttitor enim et.
+              </p>
+              <AppGender />
+            </div>
+            <div className="appointment-shedul2">
+              <AppShedul />
+            </div>
+          </div>
+          <div className="shedul-map">
+            <IframeComponent />
+          </div>
+          <Touch/>
         </div>
       </div>
     </div>
